6d9372868b92344c9440baaa8c92d4c2088788e8
MarcoPedraza <marco.pedraza.palacios@gmail.com>
2023-09-16 13:33:44 +0200

    Nuevos cambios + compilador en vscode

--- 6.Ejercicios de estructuras de seleccion/39.NumerosEnterosAromanos.c
+++ 6.Ejercicios de estructuras de seleccion/39.NumerosEnterosAromanos.c
@@ -1,1 +1,27 @@
 // Numeros enteros a romanos
+
+#include <stdio.h>
+
+int main(){
+
+
+    int numero,millar,centenas,decenas,unidades;
+
+    printf("Digita un numero\n");
+    scanf("%i",&numero);
+
+    unidades = numero%10; numero = numero/10;
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ 6.Ejercicios de estructuras de seleccion/40.ProyextoTienda.c
@@ -0,0 +1,58 @@
+//Simular una tienda con entradas y salidas del capital
+
+
+#include <stdio.h>
+
+int main(){
+
+
+int opcion;
+float agregar, retirar, saldo =3000;
+
+ printf("\t*****************************************\n");
+ printf("\t*Este es un simulador de tienda o cajero*\n");
+ printf("\t*****************************************\n");
+
+ printf("\tSelecciona una Opcion\n\n");
+ printf("\n1) Depositar dinero");
+ printf("\n2) Retirar dinero");
+ printf("\n3) Salir del cajero");
+ printf("\nOpcion seleccionada");
+ scanf("%i",&opcion);
+
+    switch (opcion)
+    {
+    case 1:
+        printf("Cuanto dinero quiere depositar?\n");
+        scanf("%f",&agregar);
+        saldo += agregar;                            // saldo = saldo + agreagr
+        printf("Su saldo actual es de %.f",saldo);
+        break;
+
+    case 2:
+
+        printf("Cuanto dinero quiere retirar?");
+        scanf("%f",&retirar);
+
+        saldo -= retirar;
+
+        if (retirar>saldo){
+
+            printf("Tu saldo disponible solo es de %.f",saldo);
+        }
+
+        else{
+            saldo -= retirar;
+            printf("Tu saldo total es de %.f",saldo);
+        }
+    
+    default:
+        break;
+    }
+
+
+
+
+
+    return 0;
+}
